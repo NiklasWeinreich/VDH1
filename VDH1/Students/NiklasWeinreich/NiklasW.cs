fca54bd5efc4b0fd34583e2e9f176946fa6280c2
Niklas Weinreich Hansen <nikl4813@elev.tec.dk>
2021-06-03 10:12:06 +0200

    Niklas Weinreich

--- VDH1/Students/NiklasWeinreich/NiklasW.cs
+++ VDH1/Students/NiklasWeinreich/NiklasW.cs
@@ -8,6 +8,6 @@ namespace VDH1.Students.NiklasWeinreich
 {
     class NiklasW
     {
-        public string FullName { get => "Niklas Weinreich"; }
+        public string FullName { get => "Niklas Weinreich"; } 
     }
 }
